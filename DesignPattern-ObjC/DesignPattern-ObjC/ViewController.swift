d9faba2d8d949a708c54bcc43556d5920c51c258
Ficow Shen <ficowshen@hotmail.com>
2021-09-07 13:55:49 +0800

    Add a project for design patterns implemented with ObjC;

---
+++ DesignPattern-ObjC/DesignPattern-ObjC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesignPattern-ObjC
+//
+//  Created by FicowShen on 2021/9/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
